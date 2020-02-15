2614e5260a56aab400258d5370b1de928fb90c35
Volodymyr <volodymyr.y.rybak@gmail.com>
2020-02-15 11:14:42 +0200

    Add info to message

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 1000; i++) {
             try {
-                Thread.sleep(1000);
-                System.out.println(i + ") Wake up !!!");
+                Thread.sleep(2000);
+                System.out.println(i + ") Wake up !!! You have the first lesson !!!");
             } catch (InterruptedException e) {
                 e.printStackTrace();
                 System.out.println("The alarm clock is broken");
