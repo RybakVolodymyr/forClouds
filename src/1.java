11fecd19fcd192fae050f4a417d727ab8a8e1e0f
Volodymyr <volodymyr.y.rybak@gmail.com>
2020-02-15 13:57:23 +0200

    add 1.java with text

---
+++ src/1.java
@@ -0,0 +1,1 @@
+//hmmmmmmmmmmmmmmmmmmmmmmmmmm
--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,11 @@ public class Main {
         for (int i = 0; i < 1000; i++) {
             try {
                 Thread.sleep(2000);
-                System.out.println(i + ") Wa");
-                System.out.println(i + ") Wake up !!!");
-                System.out.println(i + ") Wake up !!!");
+                System.out.println(i + ") Wake up !!! You have the first lesson !!!!, Good Luck");
             } catch (InterruptedException e) {
                 e.printStackTrace();
                 System.out.println("The alarm clock is broken");
             }
         }
     }
-}~
+}
