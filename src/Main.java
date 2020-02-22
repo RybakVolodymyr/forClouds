4b2dc8e55d91301fc32c10404a8263bbda06b34e
Volodymyr <volodymyr.y.rybak@gmail.com>
2020-02-22 10:40:29 +0200

    change

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
                 System.out.println(i + ") Wake up !!! You have the first lesson !!!!, Good Luck");
             } catch (InterruptedException e) {
                 e.printStackTrace();
-                System.out.println("The alarm clock is broken");
+                System.out.println("Something went wrong");
             }
         }
     }
