c4823fa4d2a9eb2413cce21d44cdda4bf981985e
Volodymyr <volodymyr.y.rybak@gmail.com>
2020-02-22 10:48:41 +0200

    change catch

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 1000; i++) {
             try {
-                Thread.sleep(5000);
+                Thread.sleep(2000);
                 System.out.println(i + ") Wake up !!! You have the first lesson !!!!, Good Luck");
             } catch (InterruptedException e) {
                 e.printStackTrace();
-                System.out.println("vova clock does not work, please buy new");
+                System.out.println("MDA, chet ne tak");
 
             }
         }
